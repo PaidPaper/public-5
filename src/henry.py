5a2966f38089d405e3d59045a063fcea67cf072e
Wanda Carlson <87515018+wanda-carlson@users.noreply.github.com>
2024-07-29 23:09:40 +0500

    Update henry.py

--- src/henry.py
+++ src/henry.py
@@ -2,4 +2,5 @@ print("Henry testing v0.8.0")
 print("Henry testing v1.11.0")
 print("Wanda testing preflight 1.11.5 - migrations")
 print("Wanda testing preflight 1.11.5 - migrations")
+print("Wanda testing preflight 1.11.5 - migrations")
 
