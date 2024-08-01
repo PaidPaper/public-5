8c285cda7e821a2aebab3c7cd1d6cb452828d48a
Wanda Carlson <87515018+wanda-carlson@users.noreply.github.com>
2024-08-02 00:15:31 +0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -8,6 +8,6 @@ if __name__ == "__main__":
     print("listings")
     
     print("listings")
-    
-print("Henry testing v0.8.0")
+    print("Henry testing v0.8.0")
+    print("Henry testing v0.8.0")
     
